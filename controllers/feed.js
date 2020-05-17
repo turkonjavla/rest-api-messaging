1d1105adfa8a9476153d1b0ebdd68b88b9559941
Vladimir Turkonja <turkonjavlada@gmail.com>
2020-05-17 22:21:58 +0200

    Add feed controller and build a mock api

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,14 @@
 exports.getPosts = (req, res) => {
   res.status(200).json({ title: 'Feed 1', message: 'This is a message' });
 };
+
+exports.createPost = (req, res) => {
+  const { title, content } = req.body;
+
+  console.log(title, content);
+  // create in db
+  return res.status(201).json({
+    message: 'Post created',
+    post: { id: new Date().toISOString(), title, content },
+  });
+};
--- routes/feed.js
+++ routes/feed.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const feedController = require('../controllers/feed');
 
 router.get('/posts', feedController.getPosts);
+router.post('/post', feedController.createPost);
 
 module.exports = router;
