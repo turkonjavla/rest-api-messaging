9f40eec25324c746fc05bc6be6e0c78e58717208
Vladimir Turkonja <turkonjavlada@gmail.com>
2020-05-20 16:10:44 +0200

    Add logic for updating posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,4 +1,6 @@
 const { validationResult } = require('express-validator');
+const fs = require('fs');
+const path = require('path');
 
 const Post = require('../models/post');
 
@@ -64,7 +66,7 @@ exports.createPost = (req, res, next) => {
 
 exports.getSinglePost = (req, res, next) => {
   const postId = req.params.postId;
-  console.log('Post id: ', postId);
+
   Post.findById(postId)
     .then(post => {
       if (!post) {
@@ -75,9 +77,66 @@ exports.getSinglePost = (req, res, next) => {
       return res.status(200).json({ message: 'Post fetched', post });
     })
     .catch(err => {
-      if (err.statusCode) {
+      if (!err.statusCode) {
         err.statusCode = 500;
       }
       next(err);
     });
+};
+
+exports.updatePost = (req, res, next) => {
+  const postId = req.params.postId;
+  const { title, content } = req.body;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed, submittd data is incorrect');
+    error.statusCode = 422;
+    throw error;
+  }
+
+  let imageUrl = req.body.image;
+
+  if (req.file) {
+    imageUrl = req.file.path;
+  }
+
+  if (!imageUrl) {
+    const error = new Error('No image provided');
+    error.statusCode = 422;
+    throw error;
+  }
+
+  Post.findById(postId)
+    .then(post => {
+      console.log(post);
+      if (!post) {
+        const error = new Error(`Couldn't find post`);
+        error.statusCode = 404;
+        throw error;
+      }
+
+      if (imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl);
+      }
+
+      post.title = title;
+      post.content = content;
+      post.imageUrl = imageUrl;
+      return post.save();
+    })
+    .then(result => {
+      res.status(200).json({ message: 'Post updated', post: result });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+const clearImage = filePath => {
+  filePath = path.join(__dirname, '..', filePath);
+  fs.unlink(filePath, err => console.error(err));
 };
--- routes/feed.js
+++ routes/feed.js
@@ -12,5 +12,15 @@ router.post(
   feedController.createPost
 );
 router.get('/post/:postId', feedController.getSinglePost);
+router.put(
+  '/post/:postId',
+  [
+    [
+      body('title').trim().isLength({ min: 5 }),
+      body('content').trim().isLength({ min: 5 }),
+    ],
+  ],
+  feedController.updatePost
+);
 
 module.exports = router;
