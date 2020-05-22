cd6539e0605cec2d16fe0d692fb9f8913caf348c
Vladimir Turkonja <turkonjavlada@gmail.com>
2020-05-22 21:26:25 +0200

    Add express-validator checks for signup route

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,24 @@ const router = express.Router();
 
 const authController = require('../controllers/auth');
 
-router.post('/signup', authController.signup);
+router.post(
+  '/signup',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Please enter a valid email')
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then(userDoc => {
+          if (userDoc) {
+            return Promise.reject('Email already exists');
+          }
+        });
+      })
+      .normalizeEmail(),
+    body('password').trim().isLength({ min: 5 }),
+    body('name').trim(),
+  ],
+  authController.signup
+);
 
 module.exports = router;
