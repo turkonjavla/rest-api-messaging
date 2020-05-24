3489912c395364cb976ffe54368d68a32af67e6c
Vladimir Turkonja <turkonjavlada@gmail.com>
2020-05-24 10:06:28 +0200

    Add async/await to auth controller

--- keys/index.js
+++ keys/index.js
@@ -6,4 +6,5 @@ module.exports = {
   MONGO_URI: process.env.MONGO_URI,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
+  BCRYPT_ROUNDS: process.env.BCRYPT_ROUNDS,
 };
